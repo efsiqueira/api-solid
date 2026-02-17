d0a90542f542c9fc49d38778e2c25301f1edfd06
efsiqueira <edu.fss14@gmail.com>
2026-02-16 22:49:52 -0300

    🐳 chore(orm): add prisma to project

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+import { PrismaPg } from "@prisma/adapter-pg"
 import fastify from "fastify"
+import { PrismaClient } from "generated/prisma/client"
+import { env } from "node:process"
 
 export const app = fastify()
+
+const connectionString = env.DATABASE_URL
+const adapter = new PrismaPg({ connectionString })
+
+const prisma = new PrismaClient({ adapter })
+
+prisma.user.create({
+  data: {
+    name: 'Eduardo Siqueira',
+    email: 'eduardo.siqueira@example.com'
+  }
+})
