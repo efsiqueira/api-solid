2061fa4acb51665c2598e6b3a8ba69f6109d3790
efsiqueira <edu.fss14@gmail.com>
2026-03-04 22:46:16 -0300

    ✨ feat: add check-in rules

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -4,4 +4,5 @@ export interface CheckInsRepository {
   // findById(id: string): Promise<CheckIn | null>
   // findByEmail(email: string): Promise<CheckIn | null>
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
 }
