fab9a3024bb6351a62c66b26797b47aa75233452
efsiqueira <edu.fss14@gmail.com>
2026-03-18 22:35:39 -0300

    ✨ feat(check-ins): add controllers

---
+++ src/http/controllers/check-ins/history.ts
@@ -0,0 +1,22 @@
+import type { FastifyRequest, FastifyReply } from "fastify"
+import z from "zod"
+import { makeFetchUserCheckInsHistoryUseCase } from "@/use-cases/factories/make-fetch-user-check-ins-history-use-case"
+
+export async function history(request: FastifyRequest, reply: FastifyReply) {
+  const checkInsHistoryQuerySchema = z.object({
+    page: z.coerce.number().min(1).default(1)
+  })
+
+  const { page } = checkInsHistoryQuerySchema.parse(request.query)
+
+  const checkInsHistoryUseCase = makeFetchUserCheckInsHistoryUseCase()
+
+  const { checkIns } = await checkInsHistoryUseCase.execute({
+    page,
+    userId: request.user.sub
+  })
+
+  return reply.status(200).send({
+    checkIns
+  })
+}
